ff913ebb236b5378751682575e39ee554579f831
Shimi <62647652+shimonna394@users.noreply.github.com>
2020-08-06 22:55:51 +0300

    change 1

---
+++ cmake-build-debug/CMakeFiles/untitled3.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/src/Action.cpp" "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/cmake-build-debug/CMakeFiles/untitled3.dir/src/Action.cpp.o"
+  "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/src/Session.cpp" "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/cmake-build-debug/CMakeFiles/untitled3.dir/src/Session.cpp.o"
+  "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/src/User.cpp" "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/cmake-build-debug/CMakeFiles/untitled3.dir/src/User.cpp.o"
+  "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/src/Watchable.cpp" "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/cmake-build-debug/CMakeFiles/untitled3.dir/src/Watchable.cpp.o"
+  "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/src/main.cpp" "/users/studs/bsc/2020/ledeitam/Downloads/204391510-205663586/cmake-build-debug/CMakeFiles/untitled3.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled3.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled3.dir/src/Action.cpp.o"
+  "CMakeFiles/untitled3.dir/src/Session.cpp.o"
+  "CMakeFiles/untitled3.dir/src/User.cpp.o"
+  "CMakeFiles/untitled3.dir/src/Watchable.cpp.o"
+  "CMakeFiles/untitled3.dir/src/main.cpp.o"
+  "untitled3"
+  "untitled3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
